f4a6167b9be76ebe9bc88b119fe4dff2f998aced
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2026-06-30 20:01:32 +0600

    fix: correct import path for IncludedSection in LandingPage and add meta function in Signup route

--- client/app/routes/signup.tsx
+++ client/app/routes/signup.tsx
@@ -1,4 +1,15 @@
 import RegistrationForm from "@/components/auth/signup-form";
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Register | Devfolio CMS" },
+    {
+      name: "description",
+      content: "A central place for your portfolio contents",
+    },
+  ];
+}
 
 const Signup = () => {
   return <RegistrationForm />;
