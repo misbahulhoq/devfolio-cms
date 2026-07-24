bb933be1921980243b5bd88e236282b4e3de7716
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2026-07-24 10:07:43 +0600

    Refactor code structure for improved readability and maintainability

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips properties not in the DTO
+      forbidNonWhitelisted: true, // throws error if extra props are sent
+      transform: true, // auto-transforms payloads to DTO instances
+    }),
+  );
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder().setTitle('My API').build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
